47b07eb07a1a84d9621398b44dcdec25629c8e6c
samir <stabordasierra@gmail.com>
2026-07-07 16:03:50 +1000

    Gifs app component created, some styles and mock data added

---
+++ src/GifsApp.tsx
@@ -0,0 +1,41 @@
+import { mockGifs } from "./mock-data/gifs.mock"
+
+export const GifsApp = () => {
+    return (
+        <>
+            <div className="content-center">
+                <h1>Gif's Searcher</h1>
+                <p>find and share the perfect gif</p>
+            </div>
+
+            <div className="search-container">
+                <input type="text" placeholder="Search gifs" />
+                <button>Search</button>
+            </div>
+
+            <div className="previous-searches">
+                <h2>Previous searches</h2>
+                <ul className="previous-searches-list">
+                    <li>Goku</li>
+                    <li>Naruto</li>
+                    <li>Snake</li>
+                    <li>Kratos</li>
+                </ul>
+            </div>
+
+            <div className="gifs-container">
+                {
+                    mockGifs.map( (gif) => (
+                        <div key={gif.id} className="gif-card">
+                            <img src={ gif.url } alt={ gif.title } />
+                            <h3>{ gif.title }</h3>
+                            <p>
+                                { gif.width }x{ gif.height } (1.5mb)
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
