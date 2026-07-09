288e33169ecb4ffb446e03607b954b4d7e2c4573
samir <stabordasierra@gmail.com>
2026-07-09 17:07:06 +1000

    PreviousSearches component created

---
+++ src/components/PreviousSearches.tsx
@@ -0,0 +1,13 @@
+export const PreviousSearches = () => {
+    return(
+        <div className="previous-searches">
+            <h2>Previous searches</h2>
+            <ul className="previous-searches-list">
+                <li>Goku</li>
+                <li>Naruto</li>
+                <li>Snake</li>
+                <li>Kratos</li>
+            </ul>
+        </div>
+    )
+}
