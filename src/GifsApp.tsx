e4ed18ba731b4bcd4aa5a98961f13c57a9a32272
samir <stabordasierra@gmail.com>
2026-07-12 19:59:40 +1000

    Some validation added in PreviousSearches component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,10 +1,26 @@
-import { CustomHeader } from "./components/CustomHeader"
-import { GifList } from "./components/GifList"
-import { PreviousSearches } from "./components/PreviousSearches"
-import { SearchBar } from "./components/SearchBar"
-import { mockGifs } from "./mock-data/gifs.mock"
+import { useState } from "react";
+import { CustomHeader } from "./components/CustomHeader";
+import { GifList } from "./components/GifList";
+import { PreviousSearches } from "./components/PreviousSearches";
+import { SearchBar } from "./components/SearchBar";
+import { mockGifs } from "./mock-data/gifs.mock";
 
 export const GifsApp = () => {
+
+    const [ previousTerms, setPreviousTerms ] = useState([]);
+
+    const handleTermClick = ( term: string = "" ) => {
+        console.log({ term });
+    }
+
+    const handleSearch = ( query: string ) => {
+        query = query.toLocaleLowerCase().trim();
+        if ( query.length === 0 ) return;
+        if ( previousTerms.includes(query)) return;
+
+        setPreviousTerms( [ query, ...previousTerms].slice(0,7) );
+    }
+
     return (
         <>
             <CustomHeader
@@ -12,9 +28,15 @@ export const GifsApp = () => {
                 description="Find and share the perfect gif"
             />
 
-            <SearchBar placeholder="Search gifs"/>
+            <SearchBar
+                placeholder="Search gifs"
+                onQuery={  handleSearch }
+            />
 
-            <PreviousSearches />
+            <PreviousSearches
+                searches={previousTerms}
+                onLabelClicked={handleTermClick}
+            />
 
             <GifList gifs={ mockGifs }/>
         </>
