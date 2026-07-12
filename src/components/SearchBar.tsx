6e847a769c50ab9c66a28d1d09c6db6f0e5bd5ff
samir <stabordasierra@gmail.com>
2026-07-12 19:56:06 +1000

    Handle states added in SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState, type KeyboardEvent } from "react";
+
 interface Props {
     placeholder?: string;
+    onQuery: ( query: string ) => void;
 }
 
-export const SearchBar = ({ placeholder = "Search" }: Props) => {
+export const SearchBar = ({ placeholder = "Search", onQuery }: Props) => {
+
+    const [ query, setQuery ] = useState("");
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            onQuery( query );
+        }, 2000);
+
+        return () => {
+            clearTimeout( timeoutId );
+        };
+    }, [ query, onQuery ]);
+
+    const handleSearch = () => {
+        onQuery( query );
+    }
+
+    const handleKeyDown = ( event: KeyboardEvent<HTMLInputElement> ) => {
+        if ( event.key === "Enter" ) {
+            handleSearch();
+        }
+    }
+
     return(
         <div className="search-container">
-            <input type="text" placeholder={ placeholder } />
-            <button>Search</button>
+            <input
+                type="text"
+                placeholder={ placeholder }
+                value={ query }
+                onChange={ (event) => setQuery(event.target.value) }
+                onKeyDown={ handleKeyDown }
+            />
+            <button onClick={ handleSearch }>Search</button>
         </div>
     )
 }
